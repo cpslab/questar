93327dfb9efb3b21ca85cfad902d23ff3c65fc3b
xAyumux <ayumu.f.1127@gmail.com>
2022-03-11 22:37:28 +0900

    create gorm init

--- server/src/common/database.go
+++ server/src/common/database.go
@@ -3,15 +3,18 @@ package common
 import (
 	"fmt"
 
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var Db *gorm.DB
 
-func Init() {
-	db, err := gorm.Open("postgres", "")
+func Init() *gorm.DB {
+	dsn := "host=localhost user=admin password=admin dbname=questar port=db sslmode=disable TimeZone=Asia/Tokyo"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		fmt.Println("error")
+		fmt.Println("DB error(Init): ", err)
 	}
 
 	Db = db
