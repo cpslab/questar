98bd8678121341855f2061767dd59c23088f5441
xAyumux <ayumu.f.1127@gmail.com>
2022-03-20 19:15:14 +0900

    Add grouping

--- server/src/main.go
+++ server/src/main.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/cpslab/questar/users"
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 func main() {
@@ -20,29 +18,6 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"messages":    "pong",
 			"testMessage": "ping",
-		})
-	})
-
-	router.GET("/testuser", users.UserRetrieve)
-	router.GET("/testdb", func(c *gin.Context) {
-		dsn := "host=localhost user=admin password=admin dbname=questar port=5332 sslmode=disable TimeZone=Asia/Tokyo"
-
-		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "error",
-				"error":   err,
-			})
-		}
-
-		db.Select("UserName", "Password").Create(&users.User{UserName: "testuser4", Password: "test4"})
-		var users []users.User
-		// result := db.Find(&users)
-		db.Find(&users)
-
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Success",
-			// "result":  result,
 		})
 	})
 
