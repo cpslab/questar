5ef73fb0dd5eca62a9635d47b36d258b4e87c5f8
xAyumux <ayumu.f.1127@gmail.com>
2022-03-20 19:14:50 +0900

    Add init_table_users

--- docker/db/initdb.d/init_test.sql
+++ docker/db/initdb.d/init_test.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     -- 後でランダムな数値に設定する
-    username VARCHAR(20) NOT NULL,
+    user_name VARCHAR(20) NOT NULL,
     password VARCHAR(30) NOT NULL
 );
-INSERT INTO users(username, password)
+INSERT INTO users(user_name, password)
 VALUES('testuser1', 'test1');
-INSERT INTO users(username, password)
+INSERT INTO users(user_name, password)
 VALUES('testuser2', 'test2');
-INSERT INTO users(username, password)
+INSERT INTO users(user_name, password)
 VALUES('testuser3', 'test3');
